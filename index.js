954cb7334b9180793b3af4bede4d732e4b1e46a6
Mahbub Maruf <mahbuburrahman8570@gmail.com>
2023-08-22 23:59:01 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+let peraOne = document.querySelector("#allinOne");
+let name = document.querySelector("#name");
+let inputOne = document.querySelector("#Email");
+let inputTwo = document.querySelector("#Pass");
+let backG = document.querySelector("div");
+
+// For Name
+name.addEventListener("focus", function () {
+  peraOne.style.display = "block";
+  peraOne.innerHTML = "Enter Full name";
+  backG.style.backgroundColor = "rgb(245, 108, 131)";
+});
+
+name.addEventListener("focusout", function (e) {
+  peraOne.style.display = "none";
+  backG.style.backgroundColor = "aqua";
+  name.value = e.target.value.toUpperCase();
+  console.log("Name: " + e.target.value);
+});
+// For Gmail
+inputOne.addEventListener("focus", function () {
+  peraOne.style.display = "block";
+  peraOne.innerHTML = "Enter Gmail";
+  backG.style.backgroundColor = "rgb(202, 108, 245)";
+});
+
+inputOne.addEventListener("focusout", function (e) {
+  peraOne.style.display = "none";
+  backG.style.backgroundColor = "aqua";
+  console.log("Email: " + e.target.value);
+});
+
+// For Password
+inputTwo.addEventListener("focus", function () {
+  peraOne.style.display = "block";
+  peraOne.innerHTML = "Enter Password";
+  backG.style.backgroundColor = "pink";
+});
+
+inputTwo.addEventListener("focusout", function (e) {
+  peraOne.style.display = "none";
+  backG.style.backgroundColor = "aqua";
+  console.log("Password: " + e.target.value);
+});
